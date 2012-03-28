4203e66ae1c0f7ebccd76b2b1867f98663752c02
Anderson de França Queiroz <anderson.f.queiroz@gmail.com>
2012-03-27 22:53:22 -0300

    Added license

--- OFFViewer/fshader.glsl
+++ OFFViewer/fshader.glsl
@@ -6,13 +6,13 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Visualizador_OFF is distributed in the hope that it will be useful,
+ * OFFViewer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Visualizador_OFF.  If not, see <http://www.gnu.org/licenses/>.
+ * along with OFFViewer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 varying vec4 color;
