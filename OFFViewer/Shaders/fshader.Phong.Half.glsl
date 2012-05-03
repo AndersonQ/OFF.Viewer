76b11389589f3b4a88d30a1d9d72e3b0acb667ac
Anderson de França Queiroz <anderson.f.queiroz@gmail.com>
2012-05-03 00:54:32 -0300

    Phong and Phong half-way reimplemented

--- OFFViewer/Shaders/fshader.Phong.Half.glsl
+++ OFFViewer/Shaders/fshader.Phong.Half.glsl
@@ -1,7 +1,6 @@
 // per-fragment interpolated values from the vertex shader
-varying  vec3 fN;
-varying  vec3 fL;
-varying  vec3 fE;
+varying vec3 v;
+varying vec3 N;
 
 uniform vec4 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform vec4 LightPosition;
@@ -9,19 +8,18 @@ uniform float Shininess;
 
 void main()
 {
-     vec3 N  = normalize(fN);
-     vec3 L  = normalize(fL);
-     vec3 E  = normalize(fE);
-     vec3 H  = normalize(L+E);
+    vec3 L = normalize(LightPosition.xyz - v);
+    vec3 E = normalize(-v);
+    //vec3 R = normalize(-reflect(L,N));
+    vec3 H = normalize(L + E);
 
-    float Kd = max(dot(N , L ), 0.0);
-    float Ks = pow(max(dot(H , N ), 0.0), Shininess);
+    vec4 Iamb = AmbientProduct;
+    vec4 Idiff = DiffuseProduct * max(dot(N,L), 0.0);
+    Idiff = clamp(Idiff, 0.0, 1.0);
 
-    vec4 diffuse  = Kd * DiffuseProduct;
-    vec4 specular = Ks * SpecularProduct;
-    vec4 ambient  = AmbientProduct;
+    vec4 Ispec = SpecularProduct * pow(max(dot(H,E),0.0),0.3 * Shininess);
+    Ispec = clamp(Ispec, 0.0, 1.0);
 
-    gl_FragColor = ambient + diffuse + specular;
+    gl_FragColor = Iamb + Idiff + Ispec;
     gl_FragColor.a = 1.0;
-
 }
--- OFFViewer/Shaders/vshader.Phong.Half.glsl
+++ OFFViewer/Shaders/vshader.Phong.Half.glsl
@@ -1,26 +1,19 @@
 attribute   vec4 vPosition;
 attribute   vec3 vNormal;
 
-varying  vec3 fN;
-varying  vec3 fE;
-varying  vec3 fL;
+varying vec3 v;
+varying vec3 N;
 
 uniform mat4 MatrixModelView;
 uniform mat3 NormalMatrix;
 uniform vec4 LightPosition;
 uniform mat4 MatrixProjection;
 
-
 void main()
 {
- gl_Position = MatrixProjection * MatrixModelView * vPosition;
-//gl_ModelViewProjectionMatrix * vPosition;//
 
- vec4 eyePosition = MatrixModelView * vPosition;
- vec4 eyeLightPos = LightPosition;
+    v = vec3(MatrixModelView * vPosition);
+    N = normalize(NormalMatrix * vNormal);
 
- fN = NormalMatrix * vNormal;
- fL = eyeLightPos.xyz - eyePosition.xyz;
- fE = -eyePosition.xyz;
-
+    gl_Position = MatrixProjection * MatrixModelView * vPosition;
 }
